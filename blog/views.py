e9b5fcab103259a0cb87d3be5996045603086be8
aashisharcade@gmail.com <aashisharcade@gmail.com>
2019-11-11 12:06:19 +0530

    database and models added

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-	{
-		'author': 'Aashish',
-		'title': 'Blog Post 1',
-		'content': 'First post content',
-		'date_posted': 'Nov 11, 2019'
-	},
-	{
-		'author': 'John Jones',
-		'title': 'Blog Post 2',
-		'content': 'Second post content',
-		'date_posted': 'Nov 15, 2019'
-	}
-	
-]
-
+from .models import Post
 
 def home(request):
 	context = {
-		'posts': posts
+		'posts': Post.objects.all()
 	}
 	return render(request, 'blog/home.html', context)
 
