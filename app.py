623d6d530ac8f49b91099101733b3d83d760ad74
aashisharcade@gmail.com <aashisharcade@gmail.com>
2019-11-15 22:36:15 +0530

    added login page

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
-post = [
+data = [
     {
         "author" : "Aashish",
         "title" : "One Down",
@@ -17,7 +17,19 @@ post = [
 
 @app.route('/')
 def index():
-    return render_template('index.html',context = post)
+    return render_template('index.html',context = data)
+
+@app.route('/login', methods=["POST","GET"])
+def login():
+    if request.method == "POST":
+        # name = request.form["nm"]
+        return redirect(url_for("user"))
+    else:
+        return render_template('login.html')
+
+@app.route('/user')
+def user():
+    return render_template('user.html',name="Aashish")
 
 if __name__ == "__main__":
     app.run(debug=True)
