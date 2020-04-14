5f18eb71dc334b886809a5ffaaca18d1f83cfbf3
the-arcade-01 <aashisharcade@gmail.com>
2020-04-14 23:43:11 +0530

    model form for create in articles

--- blogapp/articles/views.py
+++ blogapp/articles/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from . import forms
+
 
 # Create your views here.
 def article_list(request):
@@ -11,3 +14,14 @@ def article_detail(request,slug):
     # return HttpResponse(slug)
     article = Article.objects.get(slug=slug)
     return render(request,'articles/article_detail.html',{'article':article})
+
+@login_required(login_url="/accounts/login/")
+def article_create(request):
+    if request.method == 'POST':
+        form = forms.CreateArticle(request.POST,request.FILES)
+        if form.is_valid():
+            # save article to database
+            return redirect('articles:list')
+    else:
+        form = forms.CreateArticle()
+    return render(request,'articles/article_create.html',{'form':form})
