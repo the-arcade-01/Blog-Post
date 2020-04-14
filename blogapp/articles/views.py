d0b62aa19b8b5b940f02e38cbe22adff4ecda785
the-arcade-01 <aashisharcade@gmail.com>
2020-04-15 00:16:51 +0530

    login status redirecting done

--- blogapp/articles/views.py
+++ blogapp/articles/views.py
@@ -21,6 +21,9 @@ def article_create(request):
         form = forms.CreateArticle(request.POST,request.FILES)
         if form.is_valid():
             # save article to database
+            instance = form.save(commit=False)
+            instance.author = request.user
+            instance.save()
             return redirect('articles:list')
     else:
         form = forms.CreateArticle()
